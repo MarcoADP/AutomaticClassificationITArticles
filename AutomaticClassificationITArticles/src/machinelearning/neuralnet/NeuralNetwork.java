a212163f932e789eaaf2e4779bc197cb7e1a180d
Marco Aurélio Paulino <marco23_aurelio@hotmail.com>
2017-01-23 17:31:34 -0200

    BEGGINING: Neuron, Synapse and Neural

--- AutomaticClassificationITArticles/src/machinelearning/neuralnet/NeuralNetwork.java
+++ AutomaticClassificationITArticles/src/machinelearning/neuralnet/NeuralNetwork.java
@@ -6,12 +6,116 @@ import machinelearning.readers.TextReader;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class NeuralNetwork {
 
     private List<String> keyWords;
+    
+    private List<List<Neuron>> network;
+    private int numInputs;
+    private int numOutputs;
+    private int depth;
+    private Double learningRate;
+    
+    public NeuralNetwork(){
+        this.network = new ArrayList<>();
+        this.numInputs = 0;
+        this.numOutputs = 0;
+        this.learningRate = 0.0;
+    }
+    
+    public void linkNeurons(){
+        for(int i = 1; i < this.network.size(); i++){
+            for(Neuron n : this.network.get(i)){
+                n.setNumInputs(this.network.get(i-1).size());
+            }
+        }
+    }
+    
+    public void print(){
+        for(int i = 0; i < this.network.size(); i++){
+            System.out.println("LAYER #" + i);
+            for(Neuron n : this.network.get(i)){
+                n.print();
+            }
+        }
+    }
+    
+    public void reset(){
+        for(List<Neuron> list : this.network){
+            for(Neuron n : list){
+                n.resetWeights(1);
+            }
+        }
+    }
+    
+    List<Double> query(List<Double> inputs){
+        List<Double> lInp = new ArrayList<>();;
+        for(int i = 0; i < inputs.size(); i++){
+            for(int j = 0; j < this.network.get(i).size(); j++){
+                lInp.set(j, this.network.get(i).get(j).evaluate(inputs));
+            }
+        }
+        return lInp;
+    }
+    
+    public void setLearningRate(Double rate){
+        for(List<Neuron> list : this.network){
+            for(Neuron neuron : list){
+                neuron.setLearningRate(rate);
+            }
+        }
+    }
 
+    public List<String> getKeyWords() {
+        return keyWords;
+    }
+
+    public void setKeyWords(List<String> keyWords) {
+        this.keyWords = keyWords;
+    }
+
+    public List<List<Neuron>> getNetwork() {
+        return network;
+    }
+
+    public void setNetwork(List<List<Neuron>> network) {
+        this.network = network;
+    }
+
+    public int getNumInputs() {
+        return numInputs;
+    }
+
+    public void setNumInputs(int numInputs) {
+        this.numInputs = numInputs;
+    }
+
+    public int getNumOutputs() {
+        return numOutputs;
+    }
+
+    public void setNumOutputs(int numOutputs) {
+        this.numOutputs = numOutputs;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public void setDepth(int depth) {
+        this.depth = depth;
+    }
+
+    
+    
+    
+    
+    
+    
+    //Provavelmente irá usar em outra classe
     public NeuralNetwork(File keyWordsFile) {
         keyWords = TextReader.getKeyWords(keyWordsFile.getAbsolutePath());
     }
---
+++ AutomaticClassificationITArticles/src/machinelearning/neuralnet/Neuron.java
@@ -0,0 +1,146 @@
+package machinelearning.neuralnet;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Neuron {
+    
+    int numInputs;
+    Double learningRate;
+    Double zWeigth;
+    Double result;
+    Double error;
+    List<Double> lastInp;
+    List<Double> weights;
+    
+    public Neuron(){
+        this.learningRate = 0.5;
+        this.zWeigth = 0.0;
+        this.result = 0.0;
+        this.error = 0.0;
+        this.lastInp = new ArrayList<>();
+        this.weights = new ArrayList<>();
+        this.numInputs = 0;     
+    }
+
+    public Neuron(Double rate, int numInput){
+        this.learningRate = rate;
+        this.numInputs = numInput;
+        this.zWeigth = 0.0;
+        this.result = 0.0;
+        this.error = 0.0;
+        this.lastInp = new ArrayList<>();
+        this.weights = new ArrayList<>();
+        this.numInputs = 0;   
+    }
+    
+    Double evaluate(List<Double> input){
+        this.lastInp = input;
+        this.result = 0.0;
+        for(int i = 0; i < input.size(); i++){
+            this.result += input.get(i) * this.weights.get(i);
+        }
+        result = this.sigmoid(this.result + this.zWeigth);
+        return this.result;
+    }
+    
+    Double evaluate_without_save(List<Double> input){
+        Double result1 = 0.0;
+        for(int i = 0; i < input.size(); i++){
+            result1 += input.get(i) * this.weights.get(i);
+        }
+        result1 = sigmoid(result1 + this.zWeigth);
+        return result1;
+    }
+    
+    void addWeight(int id, Double w){
+        this.weights.set(id, this.weights.get(id) + w);
+    }
+    
+    void resetWeights(int range){
+        Random ran = new Random();
+        for(int i = 0; i < this.weights.size(); i++){
+            Double a = (ran.nextDouble() % (range * 200.0) - (100 * range)) / 100.0;
+            this.weights.set(i, a);
+        }
+        this.zWeigth = (ran.nextDouble() % (range * 200.0) - (100 * range)) / 100.0;
+    }
+    
+    void errorAdjust(){
+        for(int i = 0; i < this.weights.size(); i++){
+            this.weights.set(i, this.learningRate * error * this.lastInp.get(i));
+        }
+        this.zWeigth += this.learningRate * this.error;
+    }
+    
+    void print(){
+        System.out.println("Neuron: ");
+        for(int i = 0; i < this.weights.size(); i++){
+            System.out.println("WEIGHT #" + i + " => " + this.weights.get(i));
+        }
+    }
+    
+    Double sigmoid(Double num){
+        return 1.0 / (1.0 + Math.pow(2.71828, -num));
+    }
+    
+    public Double getLearningRate() {
+        return learningRate;
+    }
+
+    public void setLearningRate(Double learningRate) {
+        this.learningRate = learningRate;
+    }
+
+    public int getNumInputs() {
+        return numInputs;
+    }
+
+    public void setNumInputs(int numInputs) {
+        this.numInputs = numInputs;
+    }
+
+    public Double getzWeigth() {
+        return zWeigth;
+    }
+
+    public void setzWeigth(Double zWeigth) {
+        this.zWeigth = zWeigth;
+    }
+
+    public Double getResult() {
+        return result;
+    }
+
+    public void setResult(Double result) {
+        this.result = result;
+    }
+
+    public Double getError() {
+        return error;
+    }
+
+    public void setError(Double error) {
+        this.error = error;
+    }
+
+    public List<Double> getLastInp() {
+        return lastInp;
+    }
+
+    public void setLastInp(List<Double> lastInp) {
+        this.lastInp = lastInp;
+    }
+
+    public List<Double> getWeights() {
+        return weights;
+    }
+
+    public void setWeights(List<Double> weights) {
+        this.weights = weights;
+    }
+    
+    
+    
+}
