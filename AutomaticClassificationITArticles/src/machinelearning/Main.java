668bd0384cc5d55409cd18ee8c383eb5e39d0093
João Otávio Biondo <joao.o.b@hotmail.com>
2017-01-24 18:58:48 -0200

    Feito botão visualizar classificação

--- AutomaticClassificationITArticles/src/machinelearning/Main.java
+++ AutomaticClassificationITArticles/src/machinelearning/Main.java
@@ -7,11 +7,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 
 public class Main extends Application {
 
     public static final String TITULO = "Article Classification";
-    public static final int LARGURA = 800;
+    public static final int LARGURA = 850;
     public static final int ALTURA = 600;
 
     private Stage stage;
@@ -33,8 +35,11 @@ public class Main extends Application {
 
             Scene scene = new Scene(root);
 
+            double screenHeight = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+
             stage.setMinHeight(ALTURA);
             stage.setMinWidth(LARGURA);
+            stage.setHeight(screenHeight * 0.75);
             stage.setTitle(TITULO);
             stage.setScene(scene);
             stage.show();
