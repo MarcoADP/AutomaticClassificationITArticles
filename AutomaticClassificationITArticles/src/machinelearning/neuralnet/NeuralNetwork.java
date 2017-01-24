61988938ae6785b42453e811fccc821e91150004
Marco Aurélio Paulino <marco23_aurelio@hotmail.com>
2017-01-24 16:09:41 -0200

    UPDATE: read tokens and IA class

--- AutomaticClassificationITArticles/src/machinelearning/neuralnet/NeuralNetwork.java
+++ AutomaticClassificationITArticles/src/machinelearning/neuralnet/NeuralNetwork.java
@@ -12,24 +12,85 @@ import java.util.List;
 public class NeuralNetwork {
 
     private List<String> keyWords;
+    private List<String> classes;
+    private List<List<String>> tokens;
     
-    private List<List<Neuron>> network;
-    private int numInputs;
-    private int numOutputs;
-    private int depth;
-    private Double learningRate;
+    //private List<List<Neuron>> network;
+    //private int numInputs;
+    //private int numOutputs;
+    //private int depth;
+    //private Double learningRate;
+    //private List<Integer> countLevel;
     
-    public NeuralNetwork(){
+     public NeuralNetwork(File keyWordsFile) {
+        keyWords = TextReader.getKeyWords(keyWordsFile.getAbsolutePath());
+        this.classes = new ArrayList<>();
+        this.tokens = new ArrayList<>();
+        this.preencheClassesAndTokens();
+        this.mostraClasses();
+        this.mostraTokens();
+        
+    }
+     
+    public void mostraClasses(){
+        System.out.println("CLASSES: ");
+        for(String s : this.classes){
+            System.out.println(s);
+        }
+    }
+    
+    public void mostraTokens(){
+        for(int i = 0; i < this.tokens.size(); i++){
+            System.out.println("Classe => " + this.classes.get(i));
+            System.out.println("Tokens");
+            for(String s : this.tokens.get(i)){
+                System.out.println(s);
+            }
+            System.out.println("============================================================");
+        }
+    }
+    public void preencheClassesAndTokens(){
+        int cont = 0;
+        int k = Integer.parseInt(this.keyWords.get(cont++));
+        while(k != 0){
+            this.classes.add(this.keyWords.get(cont++));
+            List<String> list = new ArrayList<>();
+            for(int i = cont; i < cont + k; i++){
+                list.add(this.keyWords.get(i));
+            }
+            this.tokens.add(list);
+            cont += k;
+            k = Integer.parseInt(this.keyWords.get(cont++));
+        }
+    }
+
+    public String classificarTexto(File file) throws IOException {
+        List<String> pdfWords = PDFReader.getPDFWords(file);
+        
+        String filename = file.getName().replace("pdf", "txt");
+
+        int count = PDFReader.compareTexts(keyWords, pdfWords, "lista2/"+filename);
+
+        if(count > 300){
+            return "Inteligência Artificial";
+        } else {
+            return "NÃO É IA";
+        }
+    }
+    
+   /* public NeuralNetwork(){
         this.network = new ArrayList<>();
         this.numInputs = 0;
         this.numOutputs = 0;
         this.learningRate = 0.0;
+        this.countLevel = new ArrayList<>();
     }
     
     public void linkNeurons(){
-        for(int i = 1; i < this.network.size(); i++){
-            for(Neuron n : this.network.get(i)){
-                n.setNumInputs(this.network.get(i-1).size());
+        for(int i = 1; i < this.depth; i++){
+            for(int j = 0; j < this.countLevel.get(i); j++){
+                this.network.get(i).get(j).setNumInputs(this.network.get(i-1).size());
+                //n.setNumInputs(this.network.get(i-1).size());
             }
         }
     }
@@ -57,8 +118,42 @@ public class NeuralNetwork {
             for(int j = 0; j < this.network.get(i).size(); j++){
                 lInp.set(j, this.network.get(i).get(j).evaluate(inputs));
             }
+            inputs = lInp;
         }
-        return lInp;
+        return inputs;
+    }
+    
+    void train(List<Double> inputs, List<Double> expected){
+        
+        List<Double> results = this.query(inputs);
+        
+        for(int i = 0; i <= results.size(); i++){
+            this.network.get(this.network.size()-1).get(i).setError(results.get(i) * (1 - results.get(i)) * (expected.get(i) - results.get(i)));
+            this.network.get(this.network.size()-1).get(i).errorAdjust();
+        }
+        
+        for(int i = this.network.size()-2; i >= 0; i--){
+            for(int j = 0; j < this.network.size(); j++){
+                Double backPropVal = 0.0;
+                for(int k = 0; k < this.network.get(i+1).size(); k++){
+                    backPropVal += this.network.get(i+1).get(k).getWeights().get(j) * this.network.get(i+1).get(k).getError();
+                }
+                
+                this.network.get(i).get(j).setError(this.network.get(i).get(j).getResult() * (1 - this.network.get(i).get(j).getResult())*backPropVal);
+                this.network.get(i).get(j).errorAdjust();
+            }
+        }
+        
+    }
+    
+    
+    //==========================================================//
+    //                      GETs e SETs                         //  
+    //==========================================================//
+    
+    public void setNodeCountAtLevel(int count, int level){
+        //#level = count
+        this.countLevel.set(level, count);
     }
     
     public void setLearningRate(Double rate){
@@ -109,28 +204,12 @@ public class NeuralNetwork {
         this.depth = depth;
     }
 
-    
+    */
     
     
     
     
     
     //Provavelmente irá usar em outra classe
-    public NeuralNetwork(File keyWordsFile) {
-        keyWords = TextReader.getKeyWords(keyWordsFile.getAbsolutePath());
-    }
-
-    public String classificarTexto(File file) throws IOException {
-        List<String> pdfWords = PDFReader.getPDFWords(file);
-
-        String filename = file.getName().replace("pdf", "txt");
-
-        int count = PDFReader.compareTexts(keyWords, pdfWords, "lista2/"+filename);
-
-        if(count > 300){
-            return "Inteligência Artificial";
-        } else {
-            return "NÃO É IA";
-        }
-    }
+   
 }
